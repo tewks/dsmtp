759c6f2d89785c435167ffcef3fb7b580ffe0397
Thomas Greany <tg@bu.mp>
2012-02-14 17:11:03 -0800

    take kw args

--- smtp.py
+++ smtp.py
@@ -233,7 +233,7 @@ class SMTP(Client):
     default_port = SMTP_PORT
 
     def __init__(self, host='', port=0, local_hostname=None,
-                 timeout=None):
+                 timeout=None, **kw):
         """Initialize a new instance.
 
         If specified, `host' is the name of the remote host to which to
